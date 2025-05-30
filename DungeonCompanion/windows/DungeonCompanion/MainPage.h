2805b790db040fe1217025a439a3d31eaa815997
Sam Smith <samsmithdev1014@gmail.com>
2025-05-30 14:00:13 -0400

    Finished template setup and initial run

---
+++ DungeonCompanion/windows/DungeonCompanion/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::DungeonCompanion::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::DungeonCompanion::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
